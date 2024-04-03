fedb8bfff1c8835d4c78b9982080b3993b96cc4f
thanhhauatk <121245009+thanhhauatk@users.noreply.github.com>
2024-04-03 09:12:50 +0700

    Add files via upload

---
+++ maytinhcode.java
@@ -0,0 +1,24 @@
+package bt;
+
+import java.util.Scanner;
+
+public class maytinhcode {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập số thứ nhất: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Nhập số thứ hai: ");
+        //int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+
+        //System.out.println("Tổng của " + num1 + " và " + num2 + " là: " + sum);
+        System.out.print("Nhập số thứ ba: ");
+        System.out.print("Nhập số thứ tư: ");
+        System.out.print("Nhập số thứ nam: ");
+
+        scanner.close();
+    }
+}
